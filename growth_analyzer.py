9abb40ea80b1ff3fcd4534a8fe6072b9f2e38096
Jonathan Sudlow <jsudlow@gmail.com>
2012-03-24 14:33:18 -0500

    included new function in the symbols file, convereted the growth analyzer functions into a class with class functions. Cleaned up the code a bit, fixed getting the first quote from the first day I ever started. I changed the order by clause and date added to descending so we can always pull the most recent quotes locally

--- growth_analyzer.py
+++ growth_analyzer.py
@@ -2,90 +2,101 @@ import MySQLdb,datetime,re,argparse
 from symbol import *
 from database_classes import *
 from time import strftime
-
-def get_time():
-    now = datetime.datetime.now()
-    return now
-
-def get_time_yesterday(today):
-    yesterday = today - datetime.timedelta(days=1)
-    return yesterday
-
-def get_current_quote(call_sign):
-    now_string = strftime("%Y-%m-%d")
-    filter_string = "date_added like '%s" % now_string
-    filter_string += "%'"
-    for instance in session.query(Stock).filter("call_sign = '%s'" % call_sign).filter("%s" % filter_string):
-        print instance.price,instance.call_sign,instance.date_added
-        return instance.price
-def calculate_growth_rate_online(symbol):
-    future_eps = float(get_estimate_eps_next_year(symbol))
-    print "future EPS is ",future_eps
-    current_eps = float(get_earnings_per_share(symbol))    
-    print "current_eps is", current_eps
-    delta_eps = future_eps - current_eps
-    delta_eps *= 100
-    delta_eps /= current_eps
-    delta_eps_string = str(delta_eps)
-    print "The Growth Rate for %s is %s" % (symbol, delta_eps_string,)
-
-def get_quote_download_dates():
-  for instance in session.query(Stock.date_added).distinct():
-      print instance.date_added
-
-def print_portfolio():
-    get_current_quote('SCCO')
-    get_current_quote('KMP')
-    get_current_quote('MWE')
-    get_current_quote('ENB')
-
-def get_current_eps(symbol):
-    eps = float(get_earnings_per_share(symbol))
-    return eps
-
-def calculate_pe_ratio(symbol):
-    price = get_current_quote(symbol)
-    eps = get_current_eps(symbol)
-    print "eps got back is ",eps
-    pe = price/eps
-    return pe
+from datetime import date
 
 
-
-#now = get_time()
-#yesterday = get_time_yesterday(now)
-
-#now_string = str(now)
-#get_quote_download_dates()
-
-#for instance in session.query(Stock).filter("date_added>'%s'" % yesterday):
-    #print instance.call_sign
-
-#print "today",now
-#print "yesterday...hoefully:",yesterday
+class StockAnalyzer:
+    portfolio = []
+    today = date.today()
 
 
-print_portfolio()
+    def __init__(self):
+        pass
 
-#get the current growth rate for a company. The online version keys directly into the yahoo API instead of local data. Of course this will change, but I figured I would lay the blue prints
-#lets calculate the growth rate of some of the stocks I own
+    def get_time(self):
+        self.now = datetime.datetime.now()
+        return self.now
 
-print "Calculating Growth Rates...:"
-#calculate_growth_rate_online('ENB')
-#calculate_growth_rate_online('MWE')
-#calculate_growth_rate_online('SCCO')
-calculate_growth_rate_online('WAG')
-calculate_growth_rate_online('RAD')
+    def get_time_yesterday(self):
+        self.yesterday = self.today - datetime.timedelta(days=1)
+        return self.yesterday
+   
+    def get_current_quote(self,call_sign):
+        for instance in session.query(Stock).filter("call_sign = '%s'" % call_sign).order_by(desc(Stock.date_added)):
+            return instance.price
 
-pewag = calculate_pe_ratio('WAG')
-print "Walgreens PE Ratio is ",pewag
+    def calculate_growth_rate_online(self,symbol):
+        future_eps = float(get_estimate_eps_next_year(symbol))
+        current_eps = float(get_earnings_per_share(symbol))    
 
-perad = calculate_pe_ratio('RAD')
-print "Rite Aid PE Ratio is ", perad
-#get_quote_download_dates()
-#get_current_quote('KMP')
+        #Calculate the Growth Rate
+        delta_eps = future_eps - current_eps
+        delta_eps *= 100
+        delta_eps /= current_eps
+        delta_eps_string = str(delta_eps)
+
+        return delta_eps_string
+
+    def get_quote_download_dates(self):
+        for instance in session.query(Stock.date_added).distinct():
+            print instance.date_added
+
+    def print_portfolio(self):
+        print self.get_current_quote('SCCO')
+        print self.get_current_quote('KMP')
+        print self.get_current_quote('MWE')
+        print self.get_current_quote('ENB')
+
+    def get_current_eps(self,symbol):
+        eps = float(get_earnings_per_share(symbol))
+        return eps
+
+    def calculate_pe_ratio(self,symbol):
+        price = self.get_current_quote(symbol)
+        eps = self.get_current_eps(symbol)
+        
+        pe = price/eps
+
+        return pe
+
+    def calculate_snp_1yr_growth_rate(self):
+        last_year = self.today.replace( year=self.today.year - 1 )
+
+        last_year_day = last_year.replace( day = last_year.day + 1)
+    
+        string_today = self.today.strftime("%Y%m%d")
+        string_last = last_year.strftime("%Y%m%d")
+        string_last_day = last_year_day.strftime("%Y%m%d")
+
+        snp_last = get_historical_prices("^GSPC", string_last,string_last_day)
+
+        last_price = snp_last[1][4]
+        today_price = get_price("^GSPC")
+
+        net_change = float(today_price) - float(last_price)
+
+        growth_rate = net_change / float(last_price)
+
+        return growth_rate
+    
 
 
 
+#useful functions of the StockAnalyzer... more to come :)
+Analyzer = StockAnalyzer()
 
 
+print Analyzer.get_time()
+
+print Analyzer.get_time_yesterday()
+print Analyzer.get_current_quote("SCCO")
+print Analyzer.calculate_growth_rate_online("SCCO")
+
+#this function just dumps out all the dates I have to do a quick visual inspection of the indicies
+#print Analyzer.get_quote_download_dates()
+
+Analyzer.print_portfolio()
+
+print Analyzer.get_current_eps("SCCO")
+print Analyzer.calculate_pe_ratio("SCCO")
+print Analyzer.calculate_snp_1yr_growth_rate()
